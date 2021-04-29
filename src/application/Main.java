6e3c96969c0326e7e9b870042658874a77f7c3b4
jcutrer84 <80733077+jcutrer84@users.noreply.github.com>
2021-04-28 21:49:58 -0500

    Connected Flights Page

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,13 +8,17 @@ import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	
+	private static Stage theStage;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			theStage = primaryStage;
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root,800,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -24,4 +28,11 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	public static void setScene(Scene scene)
+	{
+		theStage.setScene(scene);
+		theStage.show();
+	}
+	
 }
